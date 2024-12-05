9a64ebdac5824f5266b17ec3c7dc6ad8db786045
Tobias Gilbert <tobias.gilbert@indigowavetech.com>
2024-12-04 23:09:06 -0500

    Fix Stylelint rules for indentation and string quotes

---
+++ stylelint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard" // Extends standard rules
+  ],
+  rules: {
+    "indentation": [2],           // Enforce 2-space indentation
+    "string-quotes": ["double"],  // Enforce double quotes for strings
+  },
+};
